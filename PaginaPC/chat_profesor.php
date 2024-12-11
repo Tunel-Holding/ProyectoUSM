d64bea42faf54a3763a29c52d4e2415427f3d6ac
YoshiHD4K <tomyreveroncito@gmail.com>
2024-12-10 23:07:24 -0400

    cambie el boton salir por boton atras

--- PaginaPC/chat_profesor.php
+++ PaginaPC/chat_profesor.php
@@ -232,7 +232,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['message'])) {
             </div>
         </div>
     </div>
-    <button onclick="goBack()" class="back-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>Salir</button>
+    <button onclick="goBack()" class="back-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>Atras</button>
     <h1><?php echo $_SESSION['nombremateria']?></h1>
     
     <div class="cont-chat">
--- PaginaPC/secciones.php
+++ PaginaPC/secciones.php
@@ -142,7 +142,7 @@ if (isset($_GET['valor'])) {
             </div>
         </div>
     </div>
-    <button onclick="goBack()" class="back-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>Salir</button>
+    <button onclick="goBack()" class="back-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>Atras</button>
     <h1><?php echo $_SESSION['materiaselecc']?></h1>
     <div class="materias">
     
