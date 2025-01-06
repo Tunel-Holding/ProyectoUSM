6cf59e9b8122ce4c7d22b8221856ff035a1523e3
DaniGarcia30 <daniela.aleja2021@gmail.com>
2025-01-06 13:46:34 -0400

    Se crea la estructura inicial de la página móvil con HTML y CSS, incluyendo un menú de navegación y secciones de imágenes.

--- PaginaTlf/index.php
+++ PaginaTlf/index.php
@@ -1,1 +1,46 @@
-Hola, mandame un besito miamor
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>USM</title>
+    <link rel="stylesheet" href="styles-mobile.css">
+</head>
+<body>
+     
+    <div class="menu-noticias">
+        <a href="inicio.php" class="boton-inicio">
+            <img src="logo.png" alt="Logo" class="Logo"> Iniciar Sesión
+        </a> 
+        <div class="menu-noticiasint">
+            <h1>USM</h1>
+        </div>
+    </div>
+   
+    <div class="menu-noticias1">
+        <h1>¡Visita nuestras facultades!</h1>
+    </div>
+
+    <div class="image-container">
+        <div class="image-wrapper">
+            <img src="https://bfesteticadental.com/wp-content/uploads/2017/06/Odontologia-Caracas-Venezuela-BF-Estetica-Dental.jpg" alt="Odontologia" class="rounded-image">
+        </div>
+        <div class="image-wrapper">
+            <img src="https://bfesteticadental.com/wp-content/uploads/2017/06/Odontologia-Caracas-Venezuela-BF-Estetica-Dental.jpg" alt="Odontologia" class="rounded-image">
+        </div>
+    </div>
+    <div class="image-container">
+        <div class="image-wrapper">
+            <img src="https://bfesteticadental.com/wp-content/uploads/2017/06/Odontologia-Caracas-Venezuela-BF-Estetica-Dental.jpg" alt="Odontologia" class="rounded-image">
+        </div>
+        <div class="image-wrapper">
+            <img src="https://bfesteticadental.com/wp-content/uploads/2017/06/Odontologia-Caracas-Venezuela-BF-Estetica-Dental.jpg" alt="Odontologia" class="rounded-image">
+        </div>
+    </div>
+    <div class="image-container">
+        <div class="image-wrapper">
+            <img src="https://bfesteticadental.com/wp-content/uploads/2017/06/Odontologia-Caracas-Venezuela-BF-Estetica-Dental.jpg" alt="Odontologia" class="rounded-image">
+        </div>
+    </div>
+</body>
+</html>
