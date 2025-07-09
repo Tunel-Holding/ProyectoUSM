cb9c5e647577d88c5f5cfded527c1547ebdc54e4
MaraVicky <mvggarcia05@gmail.com>
2025-07-09 19:06:13 -0400

    Arreglado el enlace entre datos y desempeño, tambien se agrego la burbuja de foto

--- llenar_datos.php
+++ llenar_datos.php
@@ -385,7 +385,7 @@ $conn->close();
             window.location.href = url;;
             // Cambia esta URL a la página de destino 
         }
-        window.onload = function() {
+         window.onload = function() {
             document.getElementById('inicio').addEventListener('click', function() {
                 redirigir('pagina_principal.php');
             });
@@ -400,6 +400,15 @@ $conn->close();
             });
             document.getElementById('chat').addEventListener('click', function() {
                 redirigir('seleccionarmateria.php');
+            });
+            document.getElementById('desempeño').addEventListener('click', function() {
+                redirigir('desempeño.php');
+            });
+            document.getElementById('notas').addEventListener('click', function() {
+                redirigir('NAlumnos.php');
+            });
+            document.getElementById('foto').addEventListener('click', function() {
+                redirigir('foto.php');
             });
         }
     </script>
