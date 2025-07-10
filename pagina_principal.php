99866e9f20fd094afebb7bae4dda86307c972856
MaraVicky <mvggarcia05@gmail.com>
2025-07-10 16:16:05 -0400

    Se acomodo un error que habia en la animacion del nuevo cuadro

--- pagina_principal.php
+++ pagina_principal.php
@@ -74,21 +74,18 @@ $resultado = $stmt->get_result();
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Noto+Sans+KR:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Inicio - USM</title>
-
     <style>
         .hover-container {
-        position: relative;
-        display: inline-block;
-        cursor: pointer;
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
         }
-
         .hover-box {
             max-height: 0;
             overflow: hidden;
             opacity: 0;
             transition: max-height 0.5s ease, opacity 0.5s ease;
         }
-
         .hover-container:hover .hover-box {
             max-height: 200px; /* ajusta según el contenido */
             opacity: 1;
