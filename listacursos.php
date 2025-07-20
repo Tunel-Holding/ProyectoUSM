624bc418e83bbc53c5dc85996b1bd64cd10bb2fc
Sebastian <sebastiansanchezar3@gmail.com>
2025-07-19 21:43:11 -0400

    Quitando mas iconos de la usm en profesores

--- listacursos.php
+++ listacursos.php
@@ -15,7 +15,8 @@ if (!isset($_SESSION['idusuario'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="css/icono.png" type="image/png">
+    <!-- <link rel="icon" href="css/icono.png" type="image/png"> -->
+    <link rel="icon" href="css/logounihubblanco.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/tablahorario.css">
     <link rel="stylesheet" href="css/principalprofesor.css">
@@ -25,7 +26,7 @@ if (!isset($_SESSION['idusuario'])) {
     <link
         href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Noto+Sans+KR:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
-    <title>Inicio - USM</title>
+    <title>Cursos - UniHub</title>
     <script src="js/control_inactividad.js"></script>
 </head>
 
--- modificar_datos_profesor.php
+++ modificar_datos_profesor.php
@@ -143,7 +143,8 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="css/icono.png" type="image/png">
+    <!-- <link rel="icon" href="css/icono.png" type="image/png"> -->
+    <link rel="icon" href="css/logounihubblanco.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/principalprofesor.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -151,7 +152,7 @@ $conn->close();
     <link
         href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Noto+Sans+KR:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
-    <title>Modificar Profesor - USM</title>
+    <title>Modificar datos - UniHub</title>
     <script src="js/control_inactividad.js"></script>
     <style>
         body.dark-mode {
