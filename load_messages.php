73103a2cb5efdc0657f1e685c133360d957230bd
Eithan <eithanbv006@gmail.com>
2025-07-20 18:42:42 -0400

    Modificar la posición del menú de puntos en load_messages.php, ajustando el valor de 'top' de 25% a 0% para mejorar la visualización.

--- load_messages.php
+++ load_messages.php
@@ -478,7 +478,7 @@ $conn->close();
         display: none;
         z-index: 9999;
         position: absolute;
-        top: 25%;
+        top: 0% !important;
         left: 0;
         right: auto;
         margin: 0;
