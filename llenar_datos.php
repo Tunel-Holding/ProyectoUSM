127c6d75ffb209275a059e213a545fb435daf6f9
Jay <jademoya0101@gmail.com>
2025-07-19 21:43:26 -0400

    mas iconos de usm

--- llenar_datos.php
+++ llenar_datos.php
@@ -156,7 +156,8 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="css/icono.png" type="image/png">
+    <!-- <link rel="icon" href="css/icono.png" type="image/png"> -->
+    <link rel="icon" href="css/logounihubblanco.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/principalalumnostyle.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
