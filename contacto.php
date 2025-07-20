31ef358ef8b7aba25ab000b674a12569f13e224d
YoshiHD4K <tomyreveroncito@gmail.com>
2025-07-19 22:44:05 -0400

    Actualiza la configuración de PHPMailer para usar STARTTLS y el puerto 587

--- contacto.php
+++ contacto.php
@@ -70,8 +70,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->SMTPAuth   = true;
             $mail->Username   = 'soportetecnico@conexiondocente.com';
             $mail->Password   = 'ZEZ^Q!@~Xgbv';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port       = 465;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port       = 587;
             $mail->SMTPOptions = [
                 'ssl' => [
                     'verify_peer' => false,
