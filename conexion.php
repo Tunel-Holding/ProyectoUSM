bbc310cdb1ac65da0385a97a016328964d260152
andreamejia <189182868+mejia1501@users.noreply.github.com>
2025-07-22 11:57:29 -0400

    review_modificarDatos

--- conexion.php
+++ conexion.php
@@ -1,27 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyectousm";
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Datos de conexión
-$servername = "199.79.62.11";
-$username   = "conexftd_conexionProfesores";
-$password   = "Lcar0n@2023";
-$dbname     = "conexftd_proyectousm";
-
-try {
-    // Crear la conexión
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    $conn->set_charset("utf8mb4");
-
-} catch (mysqli_sql_exception $e) {
-    // Mostrar mensaje si la conexión falla
-    echo "<h2>Error al conectar con la base de datos:</h2>";
-    echo "<pre>" . $e->getMessage() . "</pre>";
-    exit;
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
-//prueba pa ve cositas pq
 ?>
