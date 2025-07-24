36eeeb81a6372f62732547147b774b92ad5b4693
YoshiHD4K <tomyreveroncito@gmail.com>
2025-07-23 23:18:30 -0400

    Actualizar hoja de estilos para la vista del profesor

--- chat_profesor.php
+++ chat_profesor.php
@@ -153,7 +153,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="css/logounihubblanco.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/principalalumnostyle.css">
+    <link rel="stylesheet" href="css/principalprofesor.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
