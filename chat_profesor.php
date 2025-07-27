9f84d3b7e340f52e82fa5462abca072f3e28a798
Lui_1105 <luisangelfloresv31222633@gmail.com>
2025-07-27 12:38:55 -0400

    Solucionado problema con el icono de boton de material de consulta en el chat de profesores

--- chat_profesor.php
+++ chat_profesor.php
@@ -1781,6 +1781,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
 }`;
         document.head.appendChild(uploadMenuStyle);
     </script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
